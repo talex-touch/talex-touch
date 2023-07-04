d275c657db725773598dd839a74388fea5ca6974
TalexDreamSoul <TalexDreamSoul@Gmail.com>
2023-07-04 21:17:42 +0800

    Docs: optimize sidebar

--- app/docs/.vitepress/config/sidebar.ts
+++ app/docs/.vitepress/config/sidebar.ts
@@ -8,24 +8,24 @@ const docs = [
   { text: "Introduction", link: "/docs/documents/index" },
   { text: "Get Started", link: "/docs/documents/start" },
   { text: "Account", link: "/docs/documents/account" },
-  { text: "Customization", link: "/docs/documents/customization" },
+  // { text: "Customization", link: "/docs/documents/customization" },
   { text: "Plugin Market", link: "/docs/documents/plugin_market" },
-  { text: "Sync", link: "/docs/documents/sync" }
+  // { text: "Sync", link: "/docs/documents/sync" }
 ];
 
 const plugins = [
   { text: "What's", link: "/docs/plugins/introduction" },
   { text: "Setup", link: "/docs/plugins/setup" },
   { text: "Preload scripts", link: "/docs/plugins/preloadjs" },
-  // { text: "Events", link: "/docs/plugins/events" },
+  { text: "Events", link: "/docs/plugins/events" },
   { text: "Permission", link: "/docs/plugins/permission" },
-  { text: "Toast", link: "/docs/plugins/toast" },
-  { text: "Snippets", link: "/docs/plugins/snippets" },
+  // { text: "Toast", link: "/docs/plugins/toast" },
+  // { text: "Snippets", link: "/docs/plugins/snippets" },
   // { text: "Storage", link: "/docs/plugins/storage" },
   // { text: "I18n", link: "/docs/plugins/i18n" },
   // { text: "Theme", link: "/docs/plugins/theme" },
   // { text: "Utils", link: "/docs/plugins/utils" },
-  { text: "Widget", link: "/docs/plugins/widget" },
+  // { text: "Widget", link: "/docs/plugins/widget" },
   { text: "Publish", link: "/docs/plugins/publish" },
   {
     text: "api",
