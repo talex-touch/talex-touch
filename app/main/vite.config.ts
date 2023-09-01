498b52b30ea2800ad3a4c3a1e13e93c55d9526de
TalexDreamSoul <TalexDreamSoul@Gmail.com>
2023-09-01 11:36:03 +0800

    upd: deps versions

--- app/main/vite.config.ts
+++ app/main/vite.config.ts
@@ -1,8 +1,13 @@
+import fs from 'node:fs'
+import path from "node:path";
+import { spawn } from 'node:child_process'
+import { builtinModules } from 'node:module'
 import { defineConfig } from 'vite'
 import electron from 'vite-plugin-electron'
 import renderer from 'vite-plugin-electron-renderer'
 import vue from '@vitejs/plugin-vue'
-import path from "path";
+
+import pkg from './package.json'
 
 import vueJsx from "@vitejs/plugin-vue-jsx";
 import AutoImport from "unplugin-auto-import/vite";
@@ -14,52 +19,138 @@ import VueSetupExtend from "vite-plugin-vue-setup-extend";
 
 const basePath = path.join(__dirname, "src");
 
+const isDev = process.argv.slice(2).includes('--watch')
+const isTest = process.env.NODE_ENV === 'test'
+
 // https://vitejs.dev/config/
-export default defineConfig({
-    resolve: {
-        alias: {
-            "@modules": path.join(basePath, "modules"),
-            "@comp": path.join(basePath, "components"),
-            "@styles": path.join(basePath, "styles"),
-            "@assets": path.join(basePath, "assets"),
-            "~": basePath,
+export default defineConfig(() => {
+
+    if (!isDev && !isTest) {
+        for (const dir of ['dist', 'plugin']) {
+
+            fs.rmSync(path.join(__dirname, dir), { recursive: true, force: true })
+
+        }
+    }
+
+    const build = {
+        minify: false,
+        emptyOutDir: false,
+        outDir: 'dist',
+        lib: {
+            entry: {
+                index: 'src/main.ts',
+            },
+            formats: ['cjs', 'es'],
+            fileName: format => `[name].${format === 'es' ? 'm' : ''}js`
         },
-    },
-    plugins: [
-        vue(),
-        electron([
-            {
-                // Main-Process entry file of the Electron App.
-                entry: 'electron/index.ts'
+        rollupOptions: {
+            external: [
+                'electron',
+                'vite',
+                'vite-plugin-electron-renderer',
+                ...builtinModules,
+                ...builtinModules.map(m => `node:${m}`),
+                ...Object.keys('dependencies' in pkg ? pkg.dependencies as object : {}),
+            ],
+            output: {
+                exports: 'named',
             },
-            {
-                entry: 'electron/preload.ts',
-                onstart(options) {
-                    // Notify the Renderer-Process to reload the page when the Preload-Scripts build is complete,
-                    // instead of restarting the entire Electron App.
-                    options.reload()
-                },
+        },
+    }
+
+    return {
+        build,
+        resolve: {
+            alias: {
+                "@modules": path.join(basePath, "modules"),
+                "@comp": path.join(basePath, "components"),
+                "@styles": path.join(basePath, "styles"),
+                "@assets": path.join(basePath, "assets"),
+                "~": basePath,
             },
-        ]),
-        renderer({
-            resolve: {
-                got: {
-                    type: 'esm'
+        },
+        plugins: [
+            {
+                name: 'generate-types',
+                async closeBundle() {
+                    if (isTest) return
+
+                    removeTypes()
+                    await generateTypes()
+                    moveTypesToDist()
+                    removeTypes()
                 }
-            }
-        }),
-        // ElementPlus({
-        //     useSource: true,
-        // }),
-        Unocss(),
-        vueJsx(),
-        AutoImport({
-            resolvers: [ElementPlusResolver()],
-            imports: ["vue"],
-        }),
-        Components({
-            resolvers: [ElementPlusResolver()],
-        }),
-        VueSetupExtend(),
-    ],
+            },
+            vue(),
+            electron([
+                {
+                    // Main-Process entry file of the Electron App.
+                    entry: 'electron/index.ts'
+                },
+                {
+                    entry: 'electron/preload.ts',
+                    onstart(options) {
+                        // Notify the Renderer-Process to reload the page when the Preload-Scripts build is complete,
+                        // instead of restarting the entire Electron App.
+                        options.reload()
+                    },
+                },
+            ]),
+            renderer({
+                resolve: {
+                    got: {
+                        type: 'esm'
+                    }
+                }
+            }),
+            // ElementPlus({
+            //     useSource: true,
+            // }),
+            Unocss(),
+            vueJsx(),
+            AutoImport({
+                resolvers: [ElementPlusResolver()],
+                imports: ["vue"],
+            }),
+            Components({
+                resolvers: [ElementPlusResolver()],
+            }),
+            VueSetupExtend(),
+        ],
+    }
+
 })
+
+function removeTypes() {
+    fs.rmSync(path.join(__dirname, 'types'), { recursive: true, force: true })
+}
+
+function generateTypes() {
+    return new Promise(resolve => {
+        const cp = spawn(
+            process.platform === 'win32' ? 'npm.cmd' : 'npm',
+            ['run', 'types'],
+            { stdio: 'inherit' },
+        )
+        cp.on('exit', code => {
+            !code && console.log('[types]', 'declaration generated')
+            resolve(code)
+        })
+        cp.on('error', process.exit)
+    })
+}
+
+function moveTypesToDist() {
+    const types = path.join(__dirname, 'types')
+    const dist = path.join(__dirname, 'dist')
+    const files = fs.readdirSync(types).filter(n => n.endsWith('.d.ts'))
+    for (const file of files) {
+        const from = path.join(types, file)
+        const to = path.join(dist, file)
+        fs.writeFileSync(to, fs.readFileSync(from, 'utf8'))
+
+        const cwd = process.cwd()
+        console.log('[types]', `${path.relative(cwd, from)} -> ${path.relative(cwd, to)}`)
+    }
+}
