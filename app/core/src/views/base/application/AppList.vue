0a00e08542cf624c9745ced64d985ebad3db4297
TalexDreamSoul <2418876761@qq.com>
2024-05-04 11:06:55 +0800

    add: new order button with mention and style fixed

--- app/core/src/views/base/application/AppList.vue
+++ app/core/src/views/base/application/AppList.vue
@@ -1,4 +1,6 @@
 <script name="AppList" setup lang="ts">
+import { appAmo } from '~/views/box/search-box'
+
 const props = defineProps<{
   list: any[],
   index: number,
@@ -8,8 +10,71 @@ const emits = defineEmits<{
   (e: "select", val: any): void,
 }>()
 
-// const _list = ref(props.list)
+enum EOrderWay {
+  SORT_DESC = 0, // default
+  DIC = 1,
+  D_DIC = 2,
+  FREQ = 3
+}
+
+const _list = ref()
 const search = ref("")
+const orderWay = ref<EOrderWay>(0)
+
+watch(() => props.list, () => {
+  _list.value = [...props.list]
+
+  handleOrderWay()
+}, { immediate: true })
+
+function handleOrderWay() {
+  console.log("a", orderWay.value)
+
+  if (orderWay.value === EOrderWay.SORT_DESC) {
+    _list.value = [...props.list]
+    return
+  }
+
+  if (orderWay.value === EOrderWay.DIC) {
+    _list.value = _list.value.sort((a, b) => a.name.localeCompare(b.name))
+    return
+  }
+
+  if (orderWay.value === EOrderWay.D_DIC) {
+    _list.value = _list.value.sort((a, b) => b.name.localeCompare(a.name))
+    return
+  }
+
+  if (orderWay === EOrderWay.D_DIC) {
+    _list.value = _list.value.sort((a, b) => b.name.localeCompare(a.name))
+    return
+  }
+
+  _list.value = _list.value.sort((a, b) => {
+    const aI = appAmo[a.name]
+    const bI = appAmo[b.name]
+
+    if (aI === undefined && bI === undefined) {
+      return 0
+    }
+
+    if (aI === undefined) {
+      return 1
+    }
+
+    if (bI === undefined) {
+      return -1
+    }
+
+    return bI - aI
+  })
+}
+
+function handleOrderChange() {
+  orderWay.value = (orderWay.value + 1) % 4
+
+  handleOrderWay()
+}
 
 watch(() => search.value, val => {
   emits('search', val)
@@ -45,12 +110,19 @@ function handleClick(item: any, ind: number) {
 
 <template>
   <el-scrollbar>
-    <ul class="AppList">
+    <TransitionGroup name="list" tag="ul" class="AppList">
       <div class="AppList-Toolbox">
         <FlatInput v-model="search" placeholder="Type to search..." :fetch="search" />
+
+        <span @click="handleOrderChange" class="order-way">
+          <i v-if="orderWay === 0" class="i-ri-sort-desc" />
+          <i v-if="orderWay === 1" class="i-ri-sort-alphabet-asc" />
+          <i v-if="orderWay === 2" class="i-ri-sort-alphabet-desc" />
+          <i v-if="orderWay === 3" class="i-ri-sort-number-asc" />
+        </span>
       </div>
       <li class="fake-background" :class="{ active: index === ind }" @click="handleClick(item, ind)"
-        v-for="(item, ind) in list">
+        v-for="(item, ind) in _list">
         <img :src="item.icon" alt="">
 
         <div class="Main">
@@ -59,22 +131,47 @@ function handleClick(item: any, ind: number) {
           <!-- <p class="desc">{{ item.desc }}</p> -->
         </div>
       </li>
-    </ul>
+    </TransitionGroup>
     <div class="AppList-Info">
-      <span v-if="search">{{ list.length }} searched on this device.</span>
-      <span v-else>{{ list.length }} applications on this device.</span>
+      <span v-if="search">{{ _list.length }} searched on this device.</span>
+      <span v-else>{{ _list.length }} applications on this device.</span>
+      <span class="order">
+        <span v-if="orderWay === 0">Default Order</span>
+        <span v-if="orderWay === 1">Dictionary In</span>
+        <span v-if="orderWay === 2">Dictionary De</span>
+        <span v-if="orderWay === 3">Number Frequent</span>
+      </span>
     </div>
   </el-scrollbar>
 </template>
 
 <style lang="scss">
+.list-enter-active,
+.list-leave-active {
+  transition: all 0.5s ease;
+}
+
+.list-enter-from,
+.list-leave-to {
+  opacity: 0;
+  transform: translateX(30px);
+}
+
 .AppList-Info {
   span {
     opacity: .75;
     font-size: .8rem;
   }
+
+  .order span {
+    font-size: .7rem;
+  }
+
   position: sticky;
-  padding: .25rem .5rem;
+  display: flex;
+  padding: .25rem .75rem;
+
+  justify-content: space-between;
 
   bottom: 0;
 
@@ -82,12 +179,36 @@ function handleClick(item: any, ind: number) {
 }
 
 .AppList-Toolbox {
+
+  .order-way {
+    display: flex;
+
+    align-items: center;
+    justify-content: center;
+
+    width: 32px;
+    height: 32px;
+
+    font-size: 1.25rem;
+    background-color: var(--el-fill-color-dark);
+    border-radius: 8px;
+    cursor: pointer;
+
+    &:hover {
+      background-color: var(--el-color-primary-light-5);
+    }
+  }
+
   z-index: 100;
   position: sticky;
+  display: flex;
   padding: .25rem;
+
+  justify-content: space-between;
 
   top: 0;
 
+  gap: .5rem;
   border-radius: 8px;
   background-color: var(--el-fill-color);
 }
