1b019e8ef5c3dedd2adbbfbbaa95c24ff126d703
TalexDreamSoul <TalexDreamSoul@users.noreply.github.com>
2024-06-28 08:17:31 +0000

    deploy: 29b1193478ea718234be01db95347a38a9b5e0c9

---
+++ assets/docs_plugins_api_event.md.1bb1d8b2.lean.js
@@ -0,0 +1,1 @@
+import{_ as t,c as e,o as n,a}from"./app.302b8d76.js";const f=JSON.parse('{"title":"","description":"","frontmatter":{},"headers":[],"relativePath":"docs/plugins/api/event.md","lastUpdated":1719562566000}'),l={name:"docs/plugins/api/event.md"},i=a("",1),d=[i];function r(o,s,c,g,p,_){return n(),e("div",null,d)}const y=t(l,[["render",r]]);export{f as __pageData,y as default};
